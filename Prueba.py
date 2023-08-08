b736e48f185d9be0fe1da18fc1156ac1b42a3949
SantiOta <u20212201148@usco.edu.co>
2023-08-08 18:51:34 -0500

    Operaciones artmeticas

---
+++ Prueba.py
@@ -0,0 +1,21 @@
+import os
+os.system('cls')
+ 
+a = 10
+b = 5
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+potencia = a ** b
+modulo = a % b
+
+print("La suma de a + b es: ", suma)
+print("La resta de a - b es: ", resta)
+print("La multiplicacion de a * b es: ", multiplicacion)
+print("La division de a / b es: ", division)
+print("La potencia de a ** b es: ", potencia)
+print("El modulo de a % b es: ", modulo)
+
+os.system('pause')
