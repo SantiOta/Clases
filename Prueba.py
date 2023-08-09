36528e5f37d36c12c2d501f90a3307280200dd5c
SantiOta <u20212201148@usco.edu.co>
2023-08-08 19:05:48 -0500

    Operaciones artmeticas

--- Prueba.py
+++ Prueba.py
@@ -1,21 +1,23 @@
 import os
 os.system('cls')
  
-a = 10
-b = 5
+a = int(input("Ingrese el valor de un numero cualquiera: "))
 
-suma = a + b
-resta = a - b
-multiplicacion = a * b
-division = a / b
-potencia = a ** b
-modulo = a % b
+b = int(input("Ingrese otro valor: "))
 
-print("La suma de a + b es: ", suma)
-print("La resta de a - b es: ", resta)
-print("La multiplicacion de a * b es: ", multiplicacion)
-print("La division de a / b es: ", division)
-print("La potencia de a ** b es: ", potencia)
-print("El modulo de a % b es: ", modulo)
+suma = a + b 
+resta = a - b 
+multiplicacion = a * b 
+division = a / b 
+potencia = a ** b 
+modulo = a % b 
 
-os.system('pause')
+print("La suma de a + b es: ", suma) 
+print("La resta de a - b es: ", resta) 
+print("La multiplicacion de a * b es: ", multiplicacion)  
+print("La division de a / b es: ", division) 
+print("La potencia de a ** b es: ", potencia) 
+print("El modulo de a % b es: ", modulo)   
+ 
+os.system('pause') 
+os.system('cls')
